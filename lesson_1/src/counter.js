5e165fe8b249d3cf71156e65f2ea4e2c3f31fa74
gaou-piou <i.am.piou@gmail.com>
2025-09-19 01:29:07 +0300

    test expand counter

--- lesson_1/src/counter.js
+++ lesson_1/src/counter.js
@@ -1,9 +1,17 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+	let counter = 0
+	const setCounter = (count) => {
+		counter = count
+		element.innerHTML = `count is ${counter}`
+	}
+	element.addEventListener('click', () => setCounter(counter + 1))
+	setCounter(0)
 }
+
+const hello = (test) => {
+	alert('Hello world')
+	console.log('Hello world')
+	prompt(test)
+}
+
+hello('test')
